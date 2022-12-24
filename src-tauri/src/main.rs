f31fd49887f14b714265e36835ec3ae3d68da440
Sridaran Thoniyil <sri7thon@gmail.com>
2022-12-24 03:46:48 -0500

    Remove dead code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,6 @@
 )]
 
 use gdk::DisplayManager;
-use tauri::{LogicalPosition, LogicalSize, Manager};
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
     tauri::Builder::default()
